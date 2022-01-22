3b4743ce9737dac0d56cddc661a5b6947f3782a6
Nikita Remeslov <nikita.remeslov@binary-studio.com>
2022-01-22 19:20:22 +0200

    add chart.js and moment

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,27 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/recommended", "eslint:recommended",],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: "@babel/eslint-parser",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/script-indent": ["error", 4, {
+        "baseIndent": 1,
+    },],
+    "vue/html-indent": ["error", 4, {
+        "baseIndent": 1,
+    },],
+    "quotes": ["error", "double", { "allowTemplateLiterals": true, },],
+    "comma-dangle": ["error", {
+        "arrays": "always",
+        "objects": "always",
+        "imports": "never",
+        "exports": "never",
+        "functions": "never",
+    },],
+    "semi": ["error", "always",],
   },
 };
