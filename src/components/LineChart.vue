b77ff7d991b6cf05a44d11b7eba935861c655598
Nikita Remeslov <nikita.remeslov@binary-studio.com>
2022-01-27 10:30:01 +0200

    remove unnecessary gradient

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -31,9 +31,6 @@
             this.gradient = this.$refs.canvas
                 .getContext("2d")
                 .createLinearGradient(0, 0, 0, 450);
-            this.gradient2 = this.$refs.canvas
-                .getContext("2d")
-                .createLinearGradient(0, 0, 0, 450);
 
             this.gradient.addColorStop(0, "rgba(255, 0,0, 0.5)");
             this.gradient.addColorStop(0.5, "rgba(255, 0, 0, 0.25)");
